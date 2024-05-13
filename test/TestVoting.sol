5844a22a4148b0d06d8990c87cc596bb2d6b67c2
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2024-05-13 13:08:10 +0300

    Tests

---
+++ test/TestVoting.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.16;
+
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Voting.sol";
+
+contract TestAdoption {
+  Voting v = Voting(DeployedAddresses.Voting());
+
+  function testVote() public {
+    
+  }
+}
